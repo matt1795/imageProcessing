93e6f672caed1d878b1eb230c796d7c1f4908e71
Matthew Knight <mgk1795@gmail.com>
2017-09-18 01:31:42 -0700

    Mostly done lab1 report

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -8,15 +8,25 @@
 
 I = imread('barbara.jpg');		% Semicolon doesn't let function print
 					% to console.
-iminfo('barbara.jpg')
 
 % What types of data barbara has?
+diary('iminfoDump.txt');
+iminfo('barbara.jpg')
+diary('whosDump.txt');
 whos I
+diary('sizeDump.txt');
 size(I)
-class(I)
+diary off;
 
-% Show image
-imshow(I, [0, 1]);
+% Show barbara
+fig1 = figure;
+subplot(1,3,1); 
+imshow(I);
+subplot(1,3,2); 
+imshow(I, [50, 200]);
+subplot(1,3,3); 
+imshow(I, []);
+saveas(fig1, 'imshow.png');
 
 % Editing image
 imtool(I)
@@ -27,8 +37,18 @@ imwrite(lena, 'lena1.jpg');
 imwrite(lena, 'lena2.jpg', 'quality', 5);
 imwrite(lena, 'lena3.jpg', 'quality', 80);
 
-% Find size of each file, show all images in one figure using subplot, explain
-% quality and size
+I1 = imread('lena1.jpg');
+I2 = imread('lena2.jpg');
+I3 = imread('lena3.jpg');
+
+fig2 = figure;
+subplot(1,3,1); 
+imshow(I1);
+subplot(1,3,2); 
+imshow(I2);
+subplot(1,3,3); 
+imshow(I3);
+saveas(fig2, 'lenaCompare.png');
 
 % Task 2 - Reading Color Images
 
@@ -37,20 +57,28 @@ class(I2)
 size(I2)
 
 % Show specific pixel intensity rgb respectively
-I2(93, 180, 1)
-I2(93, 180, 2)
-I2(93, 180, 3)
+I2(93, 180)
 
 % Show image of each color channel
-imshow(I2(:, :, 1)
-imshow(I2(:, :, 2)
-imshow(I2(:, :, 3)
+fig3 = figure;
+subplot(1,3,1);
+title('Red');
+imshow(I2(:, :, 1);
+subplot(1,3,2);
+title('Green');
+imshow(I2(:, :, 2);
+subplot(1,3,3);
+title('Blue');
+imshow(I2(:, :, 3);
+saveas(fig3, 'threeChannels.png');
 
 % Save as indexed coloring
 [temp, map] = rgb2ind(I2, 32);
 imwrite(temp, map, 'fruits2.png');
 [I3, map] = imread('fruitss2.png');
-imshow(I3, map)
+fig4 = figure;
+imshow(I3, map);
+saveas(fig4, 'indexed.png');
 
 % conversion functions
 I4 = imread('onion.png')
@@ -60,7 +88,3 @@ imshow(rgb2gray(I4))
 % Quality of the image will decrease. While in some situations there is no
 % discernable difference, the quality does decrease, the human eye just cannot
 % detect it.
-
-% Task 3 - Getting familiar with the Image Processing Toolbox
-
-% For later
--- lab1/task4.m
+++ lab1/task4.m
@@ -8,5 +8,30 @@
 % cameraman.tif and normalized correlation among row intensities
 
 I = imread('cameraman.tif');
+I = im2double(I);
 row = I(164, :);
 
+fig1 = figure;
+subplot(1,2,1);
+imshow(I);
+subplot(1,2,2);
+title('Row 164 Intensity vs. Column');
+plot(row);
+xlabel('Column');
+ylabel('Intensity');
+saveas(fig1, 'rowIntensity.png');
+
+% now to correlate the row
+m = 128;
+N = 256;
+corr = zeros(1, m);
+
+for k = 1:m
+    corr(k) = sum(dot(row(k:N), row(1:N-k+1)))/(N-k+1);
+
+norm_corr = corr/max(corr);
+
+fig2 = figure;
+plot(norm_corr);
+title('Normalized Autocorrelation: Row 164');
+saveas(fig2, 'autocorrelation.png');
