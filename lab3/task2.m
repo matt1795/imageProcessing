766952692b33e120cf4e67783984ff5ac32aff1e
A00858370 <A00858370@SW01L2560-15.ad.bcit.ca>
2017-09-25 10:08:42 -0700

    lab3

---
+++ lab3/task2.m
@@ -0,0 +1,6 @@
+% Script for task 2 of lab 3
+
+% Author: Matthew Knight
+% File Name: task2.m
+% Date: 2017-09-25
+
---
+++ lab3/task4.m
@@ -0,0 +1,5 @@
+% Script for task 4 of lab 3
+
+% Author: Matthew Knight
+% File Name: task4.m
+% Date: 2017-09-25
